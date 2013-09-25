e8a89446da661a048cc370fecd0e44ec79b23e98
David M. Syzdek <syzdek@bindlebinaries.com>
2013-09-25 09:02:29 -0800

    Adjusting white space.

--- include/carrdm.h
+++ include/carrdm.h
@@ -255,13 +255,13 @@ struct carrdm_reclock_struct
 #pragma mark - Variables
 #endif
 
-_CARRDM_V carrdm_magic const carrdm_magic_value;
+_CARRDM_V carrdm_magic const  carrdm_magic_value;
 
-_CARRDM_V carrdm_definition carrdm_array_def;
-_CARRDM_V carrdm_definition carrdm_base_def;
-_CARRDM_V carrdm_definition carrdm_baselock_def;
-_CARRDM_V carrdm_definition carrdm_data_def;
-_CARRDM_V carrdm_definition carrdm_reclock_def;
+_CARRDM_V carrdm_definition   carrdm_array_def;
+_CARRDM_V carrdm_definition   carrdm_base_def;
+_CARRDM_V carrdm_definition   carrdm_baselock_def;
+_CARRDM_V carrdm_definition   carrdm_data_def;
+_CARRDM_V carrdm_definition   carrdm_reclock_def;
 
 
 //////////////////
